6c40d1668665b79b390fee4398ff37ab4196da09
Solomon Saul Themba <realsolomonsaul@gmail.com>
2023-08-14 04:36:18 -0700

    Program C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print the phrase
+ * Programming is like building a multilingual puzzle
+ * Return: Value will always be zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
